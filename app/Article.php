3ae7b4b17559c7a0be9c10e7705f4f25472dc77c
rohan <rohan@rohan-maven.me>
2016-08-31 19:01:57 +0530

    article model changes

--- app/Article.php
+++ app/Article.php
@@ -11,52 +11,52 @@ class Article extends Eloquent
     protected $collection = 'Articles';
     public $fillable = ['title', 'price', 'author'];
 
-    // private $mapProps = array(
-    //     'title' => [
-    //       'type' => 'string',
-    //       "analyzer" => "standard",
-    //     ],
-    //     'price' => [
-    //       'type' => 'string',
-    //       "analyzer" => "standard",
-    //     ],
-    //     'author' => [
-    //       'type' => 'string',
-    //       "analyzer" => "myAnalyzer",
-    //     ],
-    // ); 
+    private $mapProps = array(
+        'title' => [
+          'type' => 'string',
+          "analyzer" => "standard",
+        ],
+        'price' => [
+          'type' => 'string',
+          "analyzer" => "standard",
+        ],
+        'author' => [
+          'type' => 'string',
+          "analyzer" => "myAnalyzer",
+        ],
+    ); 
     
-    // private $customAnalyzer = array(
-    //     'myAnalyzer' => [
-    //         'type' => 'stop',
-    //         "stopwords" => [","]
-    //     ]        
-    // );
+    private $customAnalyzer = array(
+        'myAnalyzer' => [
+            'type' => 'stop',
+            "stopwords" => [","]
+        ]        
+    );
 
-    // public static function createIndexWithCustomAnalyzer($shards = null, $replicas = null)
-    // {
-    //     $instance = new static;
+    public static function createIndexWithCustomAnalyzer($shards = null, $replicas = null)
+    {
+        $instance = new static;
 
-    //     $client = $instance->getElasticSearchClient();
+        $client = $instance->getElasticSearchClient();
 
-    //     $params = [
-    //         'index' => 'default',
-    //         'body' => [
-    //             'settings' => [ 
-    //                 'number_of_shards' => $shards,
-    //                 'number_of_replicas' => $replicas,
-    //                 'analysis' => [ 
-    //                     'analyzer' => $instance->customAnalyzer,
-    //                 ]
-    //             ],
-    //             'mappings' => [ 
-    //                 'articles' => [    
-    //                     'properties' => $instance->mapProps,
-    //                 ],
-    //             ]
-    //         ]
-    //     ];
+        $params = [
+            'index' => 'default',
+            'body' => [
+                'settings' => [ 
+                    'number_of_shards' => $shards,
+                    'number_of_replicas' => $replicas,
+                    'analysis' => [ 
+                        'analyzer' => $instance->customAnalyzer,
+                    ]
+                ],
+                'mappings' => [ 
+                    'articles' => [    
+                        'properties' => $instance->mapProps,
+                    ],
+                ]
+            ]
+        ];
 
-    //     return $client->indices()->create($params);
-    // }
+        return $client->indices()->create($params);
+    }
 }
